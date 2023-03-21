7c5e3fa65d70ae35b2029bdc468d46e6c82f071b
darshil2002 <97335353+darshil2002@users.noreply.github.com>
2023-03-21 20:56:21 +0530

    post method not working have to use interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MyservService } from './allserv/myserv.service';
+import { HttpClient } from '@angular/common/http';
+import { map, pluck } from 'rxjs';
+import { Program } from './model/program';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  myFormData=[{
+
+  }];
+
+
   title = 'behaviourSubject';
+  programName='';
+  programNumber='';
+  programBudget='';
+  programDescription='';
+  canDelete=true;
+  isActive= true;
+  isVirtual=false;
+  programID= '';
+
+    myGridData: any=[];
+    isActivate:any=[];
+  ngOnInit(): void {
+    this.myservice.getProgram().pipe(pluck('programs')).subscribe(res => {
+      this.myGridData = res
+      console.log(res);
+      
+    });
+    this.myservice.getProgram().pipe(pluck('programs','isActive')).subscribe(res => {
+      this.isActivate = res
+      console.log(res);
+    });
+  }
+  constructor(private http: HttpClient, private myservice: MyservService) {}
+  showFrom=false;
+  
+  addButtonClicked(){
+    console.log('the btn is clicked')
+    this.showFrom=!this.showFrom;
+  }
+
+  submitForm(myForm:any){
+    console.log('submit button is clicked')
+    const programName=this.programName;
+    const programDescription=this.programDescription;
+    const programBudget=this.programBudget;
+    const programNumber=this.programNumber;
+    console.log(programName);
+    console.log(programDescription);
+    console.log(programNumber);
+    console.log(programBudget);
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GridModule, PagerModule } from '@syncfusion/ej2-angular-grids';
+import {FormsModule,FormControl} from '@angular/forms'
+import { Validators } from '@angular/forms';
+import { MyFormComponent } from './my-form/my-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    GridModule, PagerModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
