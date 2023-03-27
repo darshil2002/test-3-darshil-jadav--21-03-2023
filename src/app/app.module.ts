d5ee0fde8f1a4af5a37454b1112e248701673b3c
darshil2002 <97335353+darshil2002@users.noreply.github.com>
2023-03-27 10:51:21 +0530

    adding intercepter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+// import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GridModule, PagerModule } from '@syncfusion/ej2-angular-grids';
 import {FormsModule,FormControl} from '@angular/forms'
 import { Validators } from '@angular/forms';
 import { MyFormComponent } from './my-form/my-form.component';
+import {HttpClient,HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import { myint } from '../myint';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,13 @@ import { MyFormComponent } from './my-form/my-form.component';
     GridModule, PagerModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: myint,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
